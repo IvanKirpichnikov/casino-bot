c05cf8ef70c61d1662a7b0ed45ab4fbc2a5fa24c
Ivan Kirpichnikov <108197839+IvanKirpichnikov@users.noreply.github.com>
2023-07-30 23:20:47 +0300

    Изменения: - Убран PRIMARY KEY у поля 'datetime' у таблицы 'users'
    Добавления:
    - Функция 'creates' в файле 'app/infra/utils/create_tables.py
    - Поддержка logging

--- app/infra/utils/create_tables.py
+++ app/infra/utils/create_tables.py
@@ -1,25 +1,35 @@
+from logging import getLogger
+
 from asyncpg import Connection
 
 from app.core.enums.roles_type import RoleType
 
 
+logger = getLogger(__name__)
+
 async def _create_users_table(connect: Connection) -> None:
+    logger.debug('Create table %r', 'users')
     async with connect.transaction():
         await connect.execute('''
             CREATE TABLE IF NOT EXISTS users(
                 id SERIAL,
                 tid BIGINT,
                 cid BIGINT,
-                role SMALLINT REFERENCES roles(name) ON DELETE CASCADE,
+                role roles,
                 datetime TIMESTAMPTZ,
-                PRIMARY KEY(id, tid, cid, datetime)
+                PRIMARY KEY(id, tid, cid)
             );
         ''')
 
 
 async def _create_roles_enum(connect: Connection) -> None:
+    logger.debug('create type %r', 'roles')
     async with connect.transaction():
         await connect.execute('''
             CREATE TYPE roles AS ENUM $1:
         ''', RoleType.get_all()
-                              )
+        )
+
+async def creates(connect: Connection) -> None:
+    await _create_roles_enum(connect)
+    await _create_users_table(connect)
