6da6ca6005206ab82d64e16e7f5a251a2170d982
yuu <karamurzaev.usup@mail.ru>
2023-07-31 00:54:57 +0200

    removed duplicate method

--- app/infra/postgres/dao/user.py
+++ app/infra/postgres/dao/user.py
@@ -11,20 +11,6 @@ class UserDAO(AbstractUser):
     def __init__(self, connect: Connection):
         self.connect = connect
 
-    async def create_table(self) -> None:
-        connect = self.connect
-        async with connect.transaction():
-            await connect.execute('''
-                CREATE TABLE IF NOT EXISTS users(
-                    id SERIAL,
-                    tid BIGINT,
-                    cid BIGINT,
-                    role SMALLINT REFERENCES roles(name) ON DELETE CASCADE,
-                    datetime TIMESTAMPTZ,
-                    PRIMARY KEY(id, tid, cid, datetime)
-                );
-            ''')
-
     async def add(self, tid: int, cid: int, dtutc: datetime) -> None:
         connect = self.connect
         async with connect.transaction():
