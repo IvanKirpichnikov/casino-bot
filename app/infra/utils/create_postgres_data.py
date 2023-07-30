2b44b09aa7ee74d0c6b49a0f47b3acb1db6339d2
yuu <karamurzaev.usup@mail.ru>
2023-07-30 23:51:04 +0200

    referral data table added

--- app/infra/utils/create_postgres_data.py
+++ app/infra/utils/create_postgres_data.py
@@ -21,6 +21,18 @@ async def _create_users_table(connect: Connection) -> None:
         ''')
 
 
+async def _create_referral_table(connect: Connection) -> None:
+    logger.debug('Creating table %r', 'refferal')
+    async with connect.transaction():
+        await connect.execute('''
+            CREATE TABLE IF NOT EXISTS referral(
+                id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE,
+                referrers_count INT DEFAULT 0,
+                referral_link TEXT
+            );
+        ''')
+
+    
 async def _create_roles_enum(connect: Connection) -> None:
     logger.debug('Creating type %r', 'roles')
     async with connect.transaction():
@@ -32,3 +44,4 @@ async def _create_roles_enum(connect: Connection) -> None:
 async def create(connect: Connection) -> None:
     await _create_roles_enum(connect)
     await _create_users_table(connect)
+    await _create_referral_table(connect)
