7af773c36eb940ddcc064ac8bc110b3f04a3cd26
Ivan Kirpichnikov <108197839+IvanKirpichnikov@users.noreply.github.com>
2023-08-01 17:32:21 +0300

    Register all middlewares

--- app/tgbot/main.py
+++ app/tgbot/main.py
@@ -4,6 +4,7 @@ from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 from redis.asyncio import Redis
 
 from app.core.configs.config import config
+from app.tgbot.middlewares import DAOMiddleware, L10NMiddleware, ThrottlingMiddleware
 
 
 def main() -> None:
@@ -13,7 +14,7 @@ def main() -> None:
     )
     if config.BOT.skip_updates:
         await bot.delete_webhook(drop_pending_updates=True)
-
+    
     redis = Redis(
         host=config.REDIS.host,
         port=config.REDIS.port,
@@ -24,10 +25,15 @@ def main() -> None:
         redis,
         key_builder=DefaultKeyBuilder(with_destiny=True)
     )
-
+    
     dp = Dispatcher(
         storage=storage,
         events_isolation=storage.create_isolation()
     )
-
+    
+    dp.update.middleware(DAOMiddleware())
+    dp.update.middleware(L10NMiddleware())
+    dp.callback_query.middleware(ThrottlingMiddleware())
+    dp.message.middleware(ThrottlingMiddleware())
+    
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
