cffdbe993b6e39692f04d737b521e9edc155ec08
Ivan Kirpichnikov <108197839+IvanKirpichnikov@users.noreply.github.com>
2023-08-01 17:04:57 +0300

    Added file 'dao.py' and implemented 'DAOMiddleware' in 'app/tgbot/middlewares' directory

---
+++ app/tgbot/middlewares/dao.py
@@ -0,0 +1,22 @@
+from typing import Any, Awaitable, Callable, Dict
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from asyncpg import Pool
+
+from app.infra.postgres.dao import DAO
+
+
+class DAOMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        pool: Pool = data.get('pool')
+        
+        async with pool.acquire() as connect:
+            data['dao'] = DAO(connect)
+            
+            return await handler(event, data)
