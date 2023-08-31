ba7401b8f52bd78a20d2090ff29fb3f710fc8351
Ivan Kirpichnikov <mmssvvvv570@gmail.com>
2023-08-31 23:57:38 +0300

    Refactor 'create_postgres_data.py'

--- app/infra/utils/create_postgres_data.py
+++ app/infra/utils/create_postgres_data.py
@@ -1,9 +1,6 @@
 import logging
-from contextlib import suppress
 
-from asyncpg import Connection, DuplicateObjectError, Pool
-
-from app.core.enums import RolesType
+from asyncpg import Connection, Pool
 
 
 logger = logging.getLogger(__name__)
@@ -13,12 +10,12 @@ async def _create_users_table(connect: Connection) -> None:
     logger.debug('Creating table %r', 'users')
     await connect.execute('''
         CREATE TABLE IF NOT EXISTS users(
-            id SERIAL PRIMARY KEY,
-            tid BIGINT UNIQUE NOT NULL,
-            cid BIGINT UNIQUE NOT NULL,
-            language VARCHAR(2) DEFAULT 'en',
-            role ROLES,
-            datetime TIMESTAMPTZ UNIQUE NOT NULL
+            id       SERIAL      PRIMARY KEY,
+            tid      BIGINT      UNIQUE NOT NULL,
+            cid      BIGINT      UNIQUE NOT NULL,
+            language VARCHAR(2)  DEFAULT 'en',
+            datetime TIMESTAMPTZ UNIQUE NOT NULL,
+            role     TEXT
         );
     ''')
 
@@ -26,7 +23,8 @@ async def _create_users_table(connect: Connection) -> None:
 async def _create_referrers_table(connect: Connection) -> None:
     await connect.execute('''
         CREATE TABLE IF NOT EXISTS referrers(
-            id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE,
+            PRIMARY KEY (id),
+            id          INT REFERENCES users ON DELETE CASCADE,
             referrer_id INT REFERENCES referrals(id) ON DELETE CASCADE
         );
     ''')
@@ -36,25 +34,16 @@ async def _create_referrals_table(connect: Connection) -> None:
     logger.debug('Creating table %r', 'referral')
     await connect.execute('''
         CREATE TABLE IF NOT EXISTS referrals(
-            id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE,
+            PRIMARY KEY (id),
+            id              INT REFERENCES users ON DELETE CASCADE,
             referrers_count INT DEFAULT 0,
-            deep_link TEXT
+            deep_link       TEXT
         );
     ''')
 
 
-async def _create_roles_enum(connect: Connection) -> None:
-    logger.debug('Creating type %r', 'roles')
-    await connect.execute('''
-        CREATE TYPE roles AS ENUM {};
-    '''.format(tuple(RolesType.get_all()))
-    )
-
-
 async def create_postgres_data(pool: Pool) -> None:
     async with pool.acquire() as connect:
-        with suppress(DuplicateObjectError):
-            await _create_roles_enum(connect)
         await _create_users_table(connect)
         await _create_referrals_table(connect)
         await _create_referrers_table(connect)
