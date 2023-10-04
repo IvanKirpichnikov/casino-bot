f383ac9365d9709c0dab5a0f31916da7b65e4f44
Ivan Kirpichnikov <mmssvvvv570@gmail.com>
2023-10-04 22:53:04 +0300

    add: Added base DTO object

---
+++ src/application/models/dto/base.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class DataTransferObject:
+    pass
+
+
+@dataclass
+class MutableDataTransferObject:
+    pass
--- src/application/models/dto/user_language.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from src.application.models.enums import LanguagesType
-
-
-@dataclass(frozen=True)
-class UserLanguageDTO:
-    language: LanguagesType
