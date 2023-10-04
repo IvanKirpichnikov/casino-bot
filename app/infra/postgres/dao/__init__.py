eb3f3903ea9c50f48318c0c5622913670e004289
Ivan Kirpichnikov <mmssvvvv570@gmail.com>
2023-10-04 17:51:24 +0300

    create: Created directory 'app/infra/postgres'

--- app/infra/postgres/dao/__init__.py
+++ app/infra/postgres/dao/__init__.py
@@ -1,6 +1,6 @@
 
-from app.infra.db.rdb.dao.user import UserDAO
-from app.infra.db.rdb.dao.role import RoleDAO
+from app.infra.postgres.dao.user import UserDAO
+
 # from app.infra.postgres.dao.refferal import ReferralDAO
 
 __all__ = (
